fe8a09d225d2608c36e0bbaf36f0843a3af4fe86
regien <gerardomalpartidavela@gmail.com>
2018-11-23 08:34:49 -0800

    fib.s alignment problem

--- nasm_tutorial/fib.s
+++ nasm_tutorial/fib.s
@@ -6,6 +6,7 @@ _main:
 			push		rbx					; saving the value of RBX (IT'S A PRESERVED REGISTER)
 
 			mov			ecx, 90				; the counter will countdown to 0
+;			mov			r9, 90
 			xor			rax, rax			; will hold current number
 			xor			rbx, rbx			; will hold the next number
 			inc			rbx					; is originally 1
@@ -18,13 +19,15 @@ print:
 			push		rax					; caller-save register
 			push		rcx					; caller-save register
 			
-			mov			rdi, format			; set 1 parameter
+			mov			rdi, [rel format]	; set 1 parameter
 			mov			rsi, rax			; set second parameter
 			xor			rax, rax			; because printf is varargs
+;			sub			rsp, 16
 
 	; stack is already aligned bcause we pushed three 8 byte registers
 			call		_printf				; printf(format, current_number)
 
+;			add			rsp, 16
 			pop			rcx					; restore caller-save register
 			pop			rax					; restore caller-save register
 
@@ -32,6 +35,7 @@ print:
 			mov			rax, rbx			; next number is now current
 			add			rbx, rdx			; get the new next number
 			dec			ecx					; count down
+;			dec			r9
 			jnz			print
 
 			pop			rbx					; restore rbx before returing
