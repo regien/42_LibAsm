11c55ab9127b9cae15446a91d18387adf53bbba3
regien <gerardomalpartidavela@gmail.com>
2018-11-25 08:37:47 -0800

    rewriting functions

--- prototype_lib_asm/ft_strlen.s
+++ prototype_lib_asm/ft_strlen.s
@@ -1,13 +1,23 @@
 ;------------------------------------------------------------------
-; Currently not using not a .section data to declare variables
-; Neither using defines,
-; todo: use it
+;	prototype of the function:
+;			size_t		_ft_strlen(const char *str)
 ;------------------------------------------------------------------
-;default		rel
-	global			_ft_strlen
+			default		rel
+			global		_ft_strlen
 
-	section		.text
+			section		.text
 _ft_strlen:
-	mov		rax, 0 ; initializing value of rax to 0 (to use it as a counter)
-;	cmp		
+	push	rbp
+	mov		rbp, rdi ; initializing value of rax to 0 (to use it as a counter)
+
+next_char:
+	cmp		byte [rdi], 0
+	jz		finished
+	inc		rdi
+	jmp		next_char
+
+finished:
+	sub		rdi, rbp
+	mov		rax, rdi
+	pop		rbp
 	ret
