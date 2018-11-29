9ec879c2c16f5eb8f7a4cb0d13b6578a93964986
regien <gerardomalpartidavela@gmail.com>
2018-11-29 07:20:27 -0800

    progress

--- prototype_lib_asm/s_funcs/ft_strcat_v2.s
+++ prototype_lib_asm/s_funcs/ft_strcat_v2.s
@@ -1,13 +1,23 @@
+
 ;------------------------------------------------------------------
 ;	prototype of the function:
 ;			void		_ft_strcat(char *str1, const char *str2)
 ;------------------------------------------------------------------
 
-			%include		'ft_strlen.s'
+;			%include		'ft_strlen.s'
 			default			rel
 			global			_ft_strcat
 
+
+; Simplier version, just go around the string and reuse the loop
+; Second time
+
 ; use two varaibles 
 			section			.text
+
 _ft_strcat:
-			cmp
+			mov				rax, rdi			; you are gonna ret that pointer
+
+loop:											; not implementing NULL protection
+			cmp				byte [rdi], 0
+
