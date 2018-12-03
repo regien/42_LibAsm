e9147c8d160fad6ddc5568b8ef74ec3d65f28e17
regien <gerardomalpartidavela@gmail.com>
2018-12-03 11:46:17 -0800

    Done with regular, starting rigorous testing

--- prototype_lib_asm/s_funcs/ft_isalnum.s
+++ prototype_lib_asm/s_funcs/ft_isalnum.s
@@ -7,5 +7,22 @@
 				global		_ft_isalnum
 
 				section		.text
-_ft_isalnum:
-			
+
+; you store this part in rax - al <- first 8 bits
+_ft_isalnum:								; ft_isdigit part
+			xor				eax, eax
+			sub				edi, 48			; x = x - '0' 
+			cmp				edi, 9
+			setbe			al
+			add				edi, 48			; x = x + '0'
+
+; You store this part in rcx - cl <- first 8 bits
+; then you do -> or al, cl
+_ft_is_alpha:								; ft_isalpha part
+			xor				ecx, ecx
+			or				edi, 32
+			sub				edi, 97
+			cmp				edi, 25
+			setbe			cl
+			or				al, cl			; if one of them is true
+			ret
