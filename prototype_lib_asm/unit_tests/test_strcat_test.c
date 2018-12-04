41602729484a796b261a3cb772310efb1848f8c6
regien <gerardomalpartidavela@gmail.com>
2018-12-04 11:58:03 -0800

    makefile modifications and new testcases

--- prototype_lib_asm/unit_tests/test_strcat_test.c
+++ prototype_lib_asm/unit_tests/test_strcat_test.c
@@ -1,14 +1,17 @@
 #include "lib_asm.h"
 
-// not done
 
 static void		test_once(char *str, char *str2, int debug, int *status)
 {
 	char			*t1 = NULL;
 	char			*t2 = NULL;
 
-	t1 = ft_strcat(realloc(ft_strdup(str), ft_strlen()), \
-			);
+	t1 = ft_strcat(realloc(ft_strdup(str), \
+		(ft_strlen(str) + ft_strlen(str2) + 1)), \
+		str2);
+	t2 = strcat(realloc(strdup(str), \
+		(strlen(str) + strlen(str2) + 1)), \
+		str2);
 
 	if (debug == 1)
 		printf("Your ft_isalpha = |%d|, system = |%d|\n", t1, t2);
@@ -20,5 +23,6 @@ void			run_strcat_test(int debug)
 {
 	int				status = -1;
 
-	test_once(realloc(strdup("vaina"), 15), " pendejada");
+	test_once("vaina", " pendejada");
+	printf("ft_strcat: %s\n", status ? "ok" : "ko");
 }
