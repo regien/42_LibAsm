9f6836635381a824a443a1d27f0222d5d8ffdbcb
regien <gerardomalpartidavela@gmail.com>
2018-12-04 22:21:03 -0800

    writing more testcases and functions

--- prototype_lib_asm/s_funcs/ft_toupper.s
+++ prototype_lib_asm/s_funcs/ft_toupper.s
@@ -7,13 +7,11 @@
 				global			_ft_toupper
 
 				section			.text
-; possibly nulify this one
-_ft_toupper:
-				xor				eax, eax
-				or				edi, 32
-				sub				edi, 97
-				cmp				edi,
-
 _ft_toupper:
 				mov				eax, edi
-				sub				edi, 
+				and				edi, 32
+				sub				edi, 65
+				cmp				edi, 25
+				add				edi, 65
+				cmovbe			eax, edi
+				ret
