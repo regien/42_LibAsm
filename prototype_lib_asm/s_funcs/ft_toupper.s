a7899841f0babdb3706e2cc1509bf7f4ce4b5177
regien <gerardomalpartidavela@gmail.com>
2018-12-04 22:08:42 -0800

    modiying some functions

--- prototype_lib_asm/s_funcs/ft_toupper.s
+++ prototype_lib_asm/s_funcs/ft_toupper.s
@@ -2,12 +2,18 @@
 ;	prototype of the function:
 ;			int			ft_toupper(int c)
 ;------------------------------------------------------------------
+; Lower case to uppercase
 				default			rel
 				global			_ft_toupper
 
 				section			.text
+; possibly nulify this one
 _ft_toupper:
 				xor				eax, eax
 				or				edi, 32
 				sub				edi, 97
-				cmp				edi, 
+				cmp				edi,
+
+_ft_toupper:
+				mov				eax, edi
+				sub				edi, 
