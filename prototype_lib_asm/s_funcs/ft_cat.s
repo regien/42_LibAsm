036087f1eef7c05810be572d167b298b639ae55b
regien <gerardomalpartidavela@gmail.com>
2018-12-09 01:27:10 -0800

    new function files

--- prototype_lib_asm/s_funcs/ft_cat.s
+++ prototype_lib_asm/s_funcs/ft_cat.s
@@ -9,6 +9,7 @@
 ;%define		OPEN				5
 %define		CLOSE				6
 %define		READ				3
+%define		STDIN				1
 %define		SIZE_BUF			4096
 
 			default			rel
@@ -21,17 +22,31 @@ buffer:		times			SIZE_BUF db 0
 _ft_cat:
 			cmp				rdi, 0
 			jl				finished
+;			push			rdi						; preserving the FD
+; GET the size of what is gonna print and then print it
+; most of the time 4096 is way more than enough
 
 reading:
+			push			rdi						; saving FD
 			mov				eax, MACHO_SYSCALL(READ)
 			lea				esi, [rel buffer]
 			mov				edx, SIZE_BUF
-			sub				rsp, 8					; aligning stack
+;			sub				rsp, 8					; aligning stack because of push
 			syscall
-			
-writing:
+			cmp				rax, 0					; if < 0 then exit
+			jl				finished
 
+writing:
+			mov				rdx, rax				; number of bytes to print
+			mov				rdi, STDIN
+			lea				esi, [rel buffer]
+			mov				rax, MACHO_SYSCALL(WRITE)
+			syscall
+			jc				finished
+			pop				rdi						; restoring fd for read
 			add				rsp, 8
+			jmp				reading					; loop for reading
 
 finished:
+			pop				rdi						; restoring stack pointer
 			ret
