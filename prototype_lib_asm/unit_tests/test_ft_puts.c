0324a9bcdb8be466be6086cbfa1bd485a919b845
regien <gerardomalpartidavela@gmail.com>
2018-12-08 23:01:28 -0800

    new testcases

--- prototype_lib_asm/unit_tests/test_ft_puts.c
+++ prototype_lib_asm/unit_tests/test_ft_puts.c
@@ -2,9 +2,12 @@
 
 static void			test_once(char *str, int debug, int *status)
 {
+//	printf("Systems = |");
 	int				t1 = puts(str);
+//	printf("|\n");
+//	printf("yours puts = |");
 	int				t2 = ft_puts(str);
-
+//	printf("|\n");
 	if (debug == 1)
 		printf("Your ft_puts = |%d| system = |%d|\n", t2, t1);
 	if (t1 != t2)
@@ -15,9 +18,11 @@ void				run_puts_test(int debug)
 {
 	int				status = 1;
 
-	test_once("something\n", debug, &status)
-	test_once(NULL, debug, &status)
-	test_once("testing", debug, &status)
+	test_once("something", debug, &status);
+	test_once(NULL, debug, &status);
+	test_once("testing", debug, &status);
+	test_once("This is just a super giant enormous string that is suppose to print", \
+			debug, &status);
 
 	printf("ft_puts: %s\n", status == 1 ? "ok" : "ko");
 }
