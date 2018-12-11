3f0ade23189b3ca0a3933226984c4fa1fe2c646c
regien <gerardomalpartidavela@gmail.com>
2018-12-11 15:22:39 -0800

    cleaning

--- tests/variables/test.s
+++ tests/variables/test.s
@@ -7,8 +7,6 @@ msg			db			"%d", 10, 0		; new line and null terminated
 			section		.text
 _main:
 	lea		rdi,		[rel msg]
-;	xor		rsi, rsi
-;	add		qword [vaina], 16
 	add		qword [rel vaina], 16
 	mov		rsi,		[rel vaina]
 	add		rsp,		8
